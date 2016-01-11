9e9d125108efb4ae80e504a9bdcc1440559f33e9
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-11 21:22:22 +0100

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-'use strict';
-var test = require('ava');
-var arrayMaxLength = require('./');
-var arr = [1, 2, 3, 4, 5];
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.is(arrayMaxLength(arr, 3).length, 3);
-	t.is(arrayMaxLength(arr, 0).length, 0);
-	t.is(arrayMaxLength(arr, 10).length, 5);
-	t.is(arrayMaxLength([], 5).length, 0);
-	t.end();
+test(t => {
+	const arr = [1, 2, 3, 4, 5];
+
+	t.is(m(arr, 3).length, 3);
+	t.is(m(arr, 0).length, 0);
+	t.is(m(arr, 10).length, 5);
+	t.is(m([], 5).length, 0);
 });
